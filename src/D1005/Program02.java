2160ae1c814985b53230b60f180294f063b34c76
deveb1479@gmail.com <deveb1479@gmail.com>
2022-10-06 13:22:18 +0900

    add 5 weeks hw

--- src/D1005/Program02.java
+++ src/D1005/Program02.java
@@ -8,34 +8,37 @@ public class Program02
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("연산을 입력하세요.");
-        String str = scanner.nextLine();
-        String[] splits = new String[3];
-        int opPoint = 0;
-        int a, b;
-
-        for (int i = 0; i < str.length(); i++)
+        for (int t = 0; t < 4; t++)
         {
-            if (str.charAt(i) == '+' || str.charAt(i) == '-' ||
-                str.charAt(i) == '*' || str.charAt(i) == '/')
+            System.out.println("연산을 입력하세요.");
+            String str = scanner.nextLine();
+            String[] splits = new String[3];
+            int opPoint = 0;
+            int a, b;
+    
+            for (int i = 0; i < str.length(); i++)
             {
-                opPoint = i;
+                if (str.charAt(i) == '+' || str.charAt(i) == '-' ||
+                    str.charAt(i) == '*' || str.charAt(i) == '/')
+                {
+                    opPoint = i;
+                }
             }
-        }
-
-        splits[0] = str.substring(0, opPoint);
-        splits[1] = str.substring(opPoint, opPoint + 1);
-        splits[2] = str.substring(opPoint + 1, str.length());
-
-        a = Integer.parseInt(splits[0]);
-        b = Integer.parseInt(splits[2]);
-
-        switch(splits[1])
-        {
-            case "+": Add add = new Add(); add.setValue(a, b); System.out.println("연산 결과: " + add.calculate()); break;
-            case "-": Sub sub = new Sub(); sub.setValue(a, b); System.out.println("연산 결과: " + sub.calculate()); break;
-            case "*": Mul mul = new Mul(); mul.setValue(a, b); System.out.println("연산 결과: " + mul.calculate()); break;
-            case "/": Div div = new Div(); div.setValue(a, b); System.out.println("연산 결과: " + div.calculate()); break;
+    
+            splits[0] = str.substring(0, opPoint);
+            splits[1] = str.substring(opPoint, opPoint + 1);
+            splits[2] = str.substring(opPoint + 1, str.length());
+    
+            a = Integer.parseInt(splits[0]);
+            b = Integer.parseInt(splits[2]);
+    
+            switch(splits[1])
+            {
+                case "+": Add add = new Add(); add.setValue(a, b); System.out.println("연산 결과: " + add.calculate()); break;
+                case "-": Sub sub = new Sub(); sub.setValue(a, b); System.out.println("연산 결과: " + sub.calculate()); break;
+                case "*": Mul mul = new Mul(); mul.setValue(a, b); System.out.println("연산 결과: " + mul.calculate()); break;
+                case "/": Div div = new Div(); div.setValue(a, b); System.out.println("연산 결과: " + div.calculate()); break;
+            }
         }
 
         scanner.close();
