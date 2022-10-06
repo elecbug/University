fee71a98b47be0d7e7cf839631dc2b36fcc36744
deveb1479@gmail.com <deveb1479@gmail.com>
2022-10-06 13:14:06 +0900

    1005 clear

---
+++ src/D1005/Mul.java
@@ -0,0 +1,19 @@
+package D1005;
+
+public class Mul 
+{
+    private int a, b;
+
+    public Mul() {}
+
+    public void setValue(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int calculate()
+    {
+        return this.a * this.b;
+    }   
+}
--- src/D1005/Program01.java
+++ src/D1005/Program01.java
@@ -1,9 +1,21 @@
 package D1005;
 
+import java.util.Scanner;
+
 public class Program01 
 {
+    public static double toDollar(double won)
+    {
+        return won / 1403.79;
+    }
+
     public static void main(String[] args)
     {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("원화를 입력하세요.");
+        System.out.println("변환된 달러: " + toDollar(scanner.nextDouble()));
+
+        scanner.close();
     }
 }
