70679b712a703927938557529a3862bb00805945
deveb1479@gmail.com <deveb1479@gmail.com>
2022-10-19 15:17:50 +0900

    run extract

---
+++ src/D1017/Process.java
@@ -0,0 +1,45 @@
+package D1017;
+
+import java.util.Scanner;
+
+public class Process {
+    public void run()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int t = 0; t < 4; t++)
+        {
+            System.out.println("연산을 입력하세요.");
+            String str = scanner.nextLine();
+            String[] splits = new String[3];
+            int opPoint = 0;
+            int a, b;
+    
+            for (int i = 0; i < str.length(); i++)
+            {
+                if (str.charAt(i) == '+' || str.charAt(i) == '-' ||
+                    str.charAt(i) == '*' || str.charAt(i) == '/')
+                {
+                    opPoint = i;
+                }
+            }
+    
+            splits[0] = str.substring(0, opPoint);
+            splits[1] = str.substring(opPoint, opPoint + 1);
+            splits[2] = str.substring(opPoint + 1, str.length());
+    
+            a = Integer.parseInt(splits[0]);
+            b = Integer.parseInt(splits[2]);
+    
+            switch(splits[1])
+            {
+                case "+": Add add = new Add(a, b); System.out.println("연산 결과: " + add.calculate()); break;
+                case "-": Sub sub = new Sub(a, b); System.out.println("연산 결과: " + sub.calculate()); break;
+                case "*": Mul mul = new Mul(a, b); System.out.println("연산 결과: " + mul.calculate()); break;
+                case "/": Div div = new Div(a, b); System.out.println("연산 결과: " + div.calculate()); break;
+            }
+        }
+
+        scanner.close();
+    }
+}
