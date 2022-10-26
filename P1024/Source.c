2d2030b45fd7d6e30c6f4d724f30bc543d400295
deveb1479@gmail.com <deveb1479@gmail.com>
2022-10-26 10:46:54 +0900

    1024 is 221024 to 221026 class

--- P1024/Source.c
+++ P1024/Source.c
@@ -51,7 +51,73 @@ Heap* CreateHeap(int capacity, int heap_type)
 	return h;
 }
 
+void PercolateDown(Heap* h, int i) {
+	int l, r, max, temp;
+	l = LeftChild(h, i);
+	r = RightChild(h, i);
+	if (l != -1 && h->arr[l] > h->arr[i]) max = l;
+	else max = i;
+	if (r != -1 && h->arr[r] > h->arr[max]) max = r;
+	if (max != i) {
+		temp = h->arr[i];
+		h->arr[i] = h->arr[max];
+		h->arr[max] = temp;
+		PercolateDown(h, max);
+	}
+}
+
+int DeleteMax(Heap* h)
+{
+	int data;
+	if (h->count == 0)
+		return-1;
+	data = h->arr[0];
+	h->arr[0] = h->arr[h->count - 1];
+	h->count--;
+	PercolateDown(h, 0);
+	return data;
+}
+
+void PercolateUp(Heap* h, int i) {
+	int p, min, temp;
+	p = Parent(h, i);
+	if (p != -1 && h->arr[p] < h->arr[i]) min = p;
+	else min = i;
+	if (min != i) {
+		temp = h->arr[i];
+		h->arr[i] = h->arr[min];
+		h->arr[min] = temp;
+		PercolateUp(h, min);
+	}
+}
+
+void Insert(Heap* h, int data)
+{
+	if (h->count < h->capacity)
+	{
+		h->arr[h->count++] = data;
+		PercolateUp(h, h->count - 1);
+	}
+}
+
 int main() 
 {
+	Heap* heap = CreateHeap(10, 1);
 
+	for (int i = 0; i < 10; i++)
+	{
+		Insert(heap, i);
+
+		printf("insert %d: ", i);
+		for (int j = 0; j < heap->count; j++)
+			printf("%d -> ", heap->arr[j]);
+		printf("\n");
+	}
+
+	for (int i = 0; i < 10; i++) {
+		printf("delete %d: ", DeleteMax(heap));
+		for (int j = 0; j < heap->count; j++)
+			printf("%d -> ", heap->arr[j]);
+		printf("\n");
+	}
 }
