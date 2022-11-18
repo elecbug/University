4a4b6f4fb0fb583390ed4fcb47701e33c7f55740
deveb1479@gmail.com <deveb1479@gmail.com>
2022-11-18 14:16:38 +0900

    make all

---
+++ src/D1114/Ex10.java
@@ -0,0 +1,9 @@
+package D1114;
+
+public class Ex10 
+{
+    public static void main(String[] args)
+    {
+        new Frame10();
+    }    
+}
---
+++ src/D1114/Ex11.java
@@ -0,0 +1,9 @@
+package D1114;
+
+public class Ex11 
+{
+    public static void main(String[] args)
+    {
+        new Frame11();
+    }    
+}
