9556ee4255b3827d903948e091acec1708e6b904
deveb1479@gmail.com <deveb1479@gmail.com>
2023-09-13 17:03:45 +0900

    Add project files.

---
+++ UnivDB/Program.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+
+void Show()
+{
+    using (MySqlConnection connection = new MySqlConnection("server=localhost;database=univDB;uid=root;pwd=1479"))
+    {
+        connection.Open();
+
+        MySqlCommand command = new MySqlCommand("select * from student", connection);
+
+        MySqlDataReader r = command.ExecuteReader();
+
+        while (r.Read())
+        {
+            for (int i = 0; i < r.FieldCount; i++)
+            {
+                Console.Write(r[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        connection.Close();
+    }
+}
+
+void Insert()
+{
+    using (MySqlConnection connection = new MySqlConnection("server=localhost;database=univDB;uid=root;pwd=1479"))
+    {
+        connection.Open();
+
+        MySqlCommand command = new MySqlCommand("insert into student values (5558001, '김연아', '서울 서초', 4, 23, 'F', '010-1111-2222', '컴퓨터')", connection);
+
+        var r = command.ExecuteNonQuery();
+        Console.WriteLine(r);
+
+        connection.Close();
+    }
+}
