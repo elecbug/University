5ab710fabcd53f812a273f46911569ebbd94af58
deveb1479@gmail.com <deveb1479@gmail.com>
2023-09-20 17:43:02 +0900

    0920

---
+++ P0920/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace P0920
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            btnPopup = new Button();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(256, 124);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // btnPopup
+            // 
+            btnPopup.Location = new Point(258, 171);
+            btnPopup.Name = "btnPopup";
+            btnPopup.Size = new Size(94, 29);
+            btnPopup.TabIndex = 1;
+            btnPopup.Text = "button1";
+            btnPopup.UseVisualStyleBackColor = true;
+            btnPopup.Click += btnPopup_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(504, 50);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(262, 304);
+            listBox1.TabIndex = 2;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(303, 247);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(386, 296);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 4;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 453);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Controls.Add(btnPopup);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btnPopup;
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
---
+++ P0920/Form1.cs
@@ -0,0 +1,61 @@
+using P0920.univDB;
+using System.Diagnostics;
+
+namespace P0920
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPopup_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            if (form2.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = form2.textBox1.Text;
+                listBox1.Items.Add(label1.Text);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.DisplayMember = "Name";
+
+            using (UnivDbContext context = new UnivDbContext())
+            {
+                var items = context.Students.ToList();
+
+                foreach (var item in items)
+                {
+                    this.listBox1.Items.Add(item);
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var item = listBox1.SelectedItem as Student;
+            this.textBox1.Text = item.Age.ToString();
+
+            Debug.WriteLine(item.Name + " " + item.Dept);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var item = listBox1.SelectedItem as Student;
+
+            using (UnivDbContext context = new UnivDbContext())
+            {
+                var stu = context.Students.First(p => p.Id == item.Id);
+                stu.Age = int.Parse(textBox1.Text);
+
+                /// stu and item are not bindied now.
+                item.Age = int.Parse((string)textBox1.Text);
+                context.SaveChanges();
+            }
+        }
+    }
+}
