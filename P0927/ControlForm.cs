7f524207495e0830541213988b4e3305882e16c7
deveb1479@gmail.com <deveb1479@gmail.com>
2023-09-27 17:25:51 +0900

    0927 clear

---
+++ P0927/ControlForm.cs
@@ -0,0 +1,84 @@
+﻿using P0927.quizDB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P0927
+{
+    public partial class ControlForm : Form
+    {
+        private Label[] labels;
+        public TextBox[] Texts { get; private set; }
+        private Button add;
+        private Button cancel;
+
+        public ControlForm()
+        {
+            InitializeComponent();
+
+            this.labels = new Label[MainForm.Columns.Length];
+            this.Texts = new TextBox[MainForm.Columns.Length];
+
+            for (int i = 0; i < MainForm.Columns.Length; i++)
+            {
+                this.labels[i] = new Label()
+                {
+                    Parent = this,
+                    Text = MainForm.Columns[i],
+                    Location = new Point(0, i * this.ClientSize.Height / 6),
+                    Size = new Size(this.ClientSize.Width / 2, this.ClientSize.Height / 6),
+                };
+                this.Texts[i] = new TextBox()
+                {
+                    Parent = this,
+                    Location = new Point(this.ClientSize.Width / 2, i * this.ClientSize.Height / 6),
+                    Size = new Size(this.ClientSize.Width / 2, this.ClientSize.Height / 6),
+                };
+            }
+
+            this.add = new Button()
+            {
+                Parent = this,
+                Location = new Point(0, this.ClientSize.Height * 5 / 6),
+                Size = new Size(this.ClientSize.Width / 2, this.ClientSize.Height / 6),
+                Text = "ADD",
+            };
+            this.cancel = new Button()
+            {
+                Parent = this,
+                Location = new Point(this.ClientSize.Width / 2, this.ClientSize.Height * 5 / 6),
+                Size = new Size(this.ClientSize.Width / 2, this.ClientSize.Height / 6),
+                Text = "CANCEL",
+            };
+
+            this.add.Click += AddClick;
+            this.cancel.Click += CancelClick;
+        }
+
+        private void CancelClick(object? sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AddClick(object? sender, EventArgs e)
+        {
+            this.Student.Number = int.Parse(this.Texts[0].Text);
+            this.Student.Name = this.Texts[1].Text;
+            this.Student.Korean = int.Parse(this.Texts[2].Text);
+            this.Student.Math = int.Parse(this.Texts[3].Text);
+            this.Student.English = int.Parse(this.Texts[4].Text);
+
+            this.DialogResult = DialogResult.OK;
+
+            this.Close();
+        }
+
+        public Student Student { get; private set; } = new Student();
+    }
+}
---
+++ P0927/quizDB/Quiz1Context.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace P0927.quizDB;
+
+public partial class Quiz1Context : DbContext
+{
+    public Quiz1Context()
+    {
+    }
+
+    public Quiz1Context(DbContextOptions<Quiz1Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("server=localhost;database=quiz1;uid=root;pwd=1479");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Number).HasName("PRIMARY");
+
+            entity.ToTable("student");
+
+            entity.Property(e => e.Number).HasColumnName("number");
+            entity.Property(e => e.English).HasColumnName("english");
+            entity.Property(e => e.Korean).HasColumnName("korean");
+            entity.Property(e => e.Math).HasColumnName("math");
+            entity.Property(e => e.Name)
+                .HasMaxLength(20)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
