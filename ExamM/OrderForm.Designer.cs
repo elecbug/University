1d632586ac9383ef3b6655f2b9628dbde11d10f6
deveb1479@gmail.com <deveb1479@gmail.com>
2023-10-25 17:31:41 +0900

    middle term exam

---
+++ ExamM/OrderForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ExamM
+{
+    partial class OrderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dateTimePicker1 = new DateTimePicker();
+            richTextBox1 = new RichTextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(12, 12);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 27);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 45);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(250, 32);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 83);
+            button1.Name = "button1";
+            button1.Size = new Size(250, 29);
+            button1.TabIndex = 2;
+            button1.Text = "완료";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // OrderForm
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(274, 132);
+            Controls.Add(button1);
+            Controls.Add(richTextBox1);
+            Controls.Add(dateTimePicker1);
+            Name = "OrderForm";
+            Text = "OrderForm";
+            Load += OrderForm_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private RichTextBox richTextBox1;
+        private Button button1;
+    }
+}
