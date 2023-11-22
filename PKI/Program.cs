d96a1f7807c43e6e48352015b8be37fd09a173a2
deveb1479@gmail.com <deveb1479@gmail.com>
2023-11-22 15:49:37 +0900

    .

--- PKI/Program.cs
+++ PKI/Program.cs
@@ -1,17 +1,11 @@
-namespace PKI
+﻿public class Program
 {
-    internal static class Program
+    public static void Main(string[] args)
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new LoginForm());
-        }
+        Console.WriteLine("Hello, Is started PKI service simulator.");
+        Console.WriteLine("What do you want to do role?");
+        Console.WriteLine("1. CA:       The trust PKI system.");
+        Console.WriteLine("2. User:     User who receive normally service.");
+        Console.WriteLine("3. Observer: Role that can secretly collect all packets and send fake packets (like Trudy)");
     }
 }
